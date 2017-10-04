01958fc13821c893bd7ecf70779ce853a0e8c5cf
Octavio Cubillos <ocubillosj@gmail.com>
2017-10-04 14:14:25 -0300

    Agregar notas

--- ModuleReports/urls.py
+++ ModuleReports/urls.py
@@ -3,7 +3,7 @@ from .views import ReportQualifications, EnrollmentReport
 
 # Normal Url
 urlpatterns = [
-    url(r'^qualifications/(?P<pk>\d+)$', ReportQualifications.as_view(), name='report-qualifications'),
+    url(r'^qualifications/(?P<id>\d+)$', ReportQualifications.as_view(), name='report-qualifications'),
 
     url(r'^enrollment/(?P<id>\d+)$', EnrollmentReport.as_view(), name='enrollment-report'),
 
--- ModuleReports/views.py
+++ ModuleReports/views.py
@@ -6,6 +6,7 @@ from ModuleCommon.models import Qualification
 from django.views import View
 import  json
 import requests
+from decimal import Decimal
 from ERP_College.settings import WEBService
 
 
@@ -13,18 +14,69 @@ class ReportQualifications(View):
     template = 'ModuleReports/report-qualifications.html'
 
     def get(self, request, *args, **kwargs):
-        """
-        r = requests.get('http://127.0.0.1:8888/api/quali/'+kwargs['pk']+'/')
-        json = r.json()
-        qualifications= json
-        for quali in json:
-            print(quali['teacher_subject'])
 
-        data = {
-            'qualifications': qualifications,
-                }
-                """
+        subject = requests.get(WEBService + 'subject')
+        subject = subject.json()
+        quali = requests.get(WEBService+'quali_id/'+kwargs['id']+'/')
+        quali = quali.json()
+        student = requests.get(WEBService+'student_id/'+kwargs['id']+'/')
+        student = student.json()
+        enrollment = requests.get(WEBService+'enrollment_st/'+kwargs['id']+'/')
+        enrollment = enrollment.json()
+        grade = requests.get(WEBService+'grade/'+str(enrollment['grade'])+'/')
+        grade = grade.json()
+        teacher = requests.get(WEBService+'teacher_id/'+str(grade['teacher'])+'/')
+        teacher = teacher.json()
+        for qua in quali:
+            print(qua)
+        quali_ = []
+        qualis = []
+        i = 0
+        for ll in subject:
+            quali_ =[{'id': ll['id'],
+                      'name': ll['name'],
+                         'ps': '',
+                         'pf': '',
+                         }]
+
+
+            o = 0
+
+            while o < 10:
+                o += 1
+                quali_[i][o] = ''
+
+            for qua in quali:
+                if qua['teacher_subject'] == ll['id']:
+                    quali_[i][qua['position']] = qua['value']
+
+            o = 0
+            sm = 0
+            pm = 0
+            while o < 10:
+                o += 1
+                if quali_[i][o] != '':
+                    sm += Decimal(quali_[i][o])
+                    pm += 1
+            if pm != 0:
+                quali_[i]['ps'] = round(float(sm / pm), 1)
+                quali_[i]['pf'] = round(float(sm / pm), 1)
+
+            #i += 1
+            print('i ='+str(i))
+            qualis+=quali_
+        i=0
+        su=0
+        for lk in qualis:
+            if lk['ps']!='':
+                su+= Decimal(lk['ps'])
+                i+=1
+            print(lk)
+        su= round(float(su / i), 1)
+        print(su)
         return render(request, self.template, locals())
+
+
 
 class EnrollmentReport(View):
     template = 'ModuleReports/enrollment.html'
